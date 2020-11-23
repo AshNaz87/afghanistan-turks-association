a270ee047267d006d5b4576491ceff5c44d821a5
Ashraf Nazar <ashraf.caspian@gmail.com>
2020-11-23 17:55:22 +0000

    Make menu links as standalone JS file

---
+++ src/util/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    path: "/",
+    title: "Home",
+    id: 0,
+  },
+  {
+    path: "/about",
+    title: "About",
+    id: 1,
+  },
+  {
+    path: "/blog",
+    title: "Blog",
+    id: 2,
+  },
+  {
+    path: "/contact",
+    title: "Contact",
+    id: 3,
+  },
+]
