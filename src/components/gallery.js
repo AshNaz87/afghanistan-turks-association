eb7fb90b72c4e8cd4851f731ca5bb77fdd6f8e0e
Ashraf Nazar <ashraf.caspian@gmail.com>
2020-12-06 18:28:33 +0000

    Add alt attribute to images in gallery section

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -9,6 +9,7 @@ const Gallery = () => (
         source: allFile(filter: { absolutePath: { regex: "/gallery/" } }) {
           edges {
             node {
+              relativePath
               childImageSharp {
                 fluid(maxWidth: 480, maxHeight: 380, quality: 80) {
                   ...GatsbyImageSharpFluid
@@ -25,9 +26,18 @@ const Gallery = () => (
     render={data => (
       <div className="row">
         {data.source.edges.map(({ node }, index) => (
-          <div className="col-12 col-md-4">
+          <div className="col-12 col-md-4" key={index}>
             <div className="card mb-4">
-              <Img key={index} fluid={node.childImageSharp.fluid} />
+              <Img
+                fluid={node.childImageSharp.fluid}
+                alt={node.relativePath
+                  .split("/")
+                  .join("")
+                  .split(".jpg")
+                  .join("")
+                  .split("-")
+                  .join(" ")}
+              />
             </div>
           </div>
         ))}
